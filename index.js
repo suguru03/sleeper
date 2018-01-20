0587fd833b4a70744795150b1f4ee85a068c7981
Suguru Motegi <suguru.motegi@gmail.com>
2018-01-19 17:54:36 -0800

    Add console.log

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const uri = process.env.SLACK_URI || require('./config').slack.uri;
 const { getArticles } = require('./scrape');
 
 (async () => {
+  console.log('Getting articles...');
   const list = await getArticles();
+  console.log('all list', list);
   const { href, innerText } = _.sample(list);
   const text = `今日の記事:「${innerText}」\n${href}`;
+  console.log(text);
   const opts = {
     method: 'POST',
     uri,
