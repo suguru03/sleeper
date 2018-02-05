4ae276fd8735ac067cc55d4c1df5061bd5a8f2bf
Suguru Motegi <suguru.motegi@gmail.com>
2018-02-04 16:15:04 -0800

    Update pull request sh and ci config

--- pull-request.sh
+++ pull-request.sh
@@ -9,9 +9,21 @@ HISTORY_PATH=$PWD/$HISTORY_FILENAME
 echo "Adding history... $1"
 echo $1 >> $HISTORY_PATH
 
+which node
+which go
+which ruby
+node -v
+npm -v
+go version
+ruby -v
+
+sudo ln -s /usr/local/go/bin/go /usr/bin/go
+sudo which go
+
 git clone https://github.com/github/hub.git
 cd hub
 git fetch --tags
-git checkout v2.2.9
-./script/build
+git checkout -b v2.2.9
+sudo make install
+ls -a
 which hub
